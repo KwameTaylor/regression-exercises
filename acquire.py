783d46d253b10642be040a29660a9377f36cce71
Kwame Taylor <maxkwametaylor@gmail.com>
2020-09-24 11:43:52 -0500

    update acquire functions

--- acquire.py
+++ acquire.py
@@ -9,32 +9,23 @@ from env import host, user, password
 def get_connection(db, user=user, host=host, password=password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
-def get_titanic_data():
-    if os.path.isfile('titanic.csv') == False:
-        sql_query = 'SELECT * FROM passengers'
-        df = pd.read_sql(sql_query, get_connection('titanic_db'))
-        df.to_csv('titanic.csv')
-        return df
-    else:
-        df = pd.read_csv('titanic.csv', index_col=0)
-        return df
+def new_mall_data():
+    '''
+    This function reads the mall customer data from the Codeup database into a df,
+    writes it to a csv file, and returns the df.
+    '''
+    sql_query = 'SELECT * FROM customers'
+    df = pd.read_sql(sql_query, get connection('mall_customers'))
+    df.to_csv('mall_customers_df.csv')
+    return df
 
-def get_iris_data():
-    if os.path.isfile('iris.csv') == False:
-        sql_query = """
-                    SELECT species_id,
-                    species_name,
-                    sepal_length,
-                    sepal_width,
-                    petal_length,
-                    petal_width
-                    FROM measurements
-                    JOIN species
-                    USING(species_id)
-                    """
-        df = pd.read_sql(sql_query, get_connection('iris_db'))
-        df.to_csv('iris.csv')
-        return df
+def get_mall_data(cached=False):
+    '''
+    This function reads in the mall customer from Codeup database if cached == False
+    or if cached == True reads in mall customer df from a csv file, returns df
+    '''
+    if cached or os.path.isfile('mall_customers_df.csv') == False:
+        df = new_mall_data()
     else:
-        df = pd.read_csv('iris.csv', index_col=0)
-        return df
+        df = pd.read_csv('mall_customers_df.csv', index_col=0)
+    return df
